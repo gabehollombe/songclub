d4b86325dfc775f52c99d54e73d57a0dd8e03764
Gabe Hollombe <gabe@avantbard.com>
2010-07-29 21:37:40 +1000

    make test read easier

--- fs_crawler/fs_crawler_test.py
+++ fs_crawler/fs_crawler_test.py
@@ -3,20 +3,30 @@ from fs_crawler import *
 
 class TestAlbumInfo(unittest.TestCase):
   def test_get_album_info_from_path(self):
+    '''
+    test_cases is a dictionary we'll use to test get_album_info_from_path(str)
+    key: sample input string
+    val: expected AlbumInfo named tupple that we should get for the given string input
+    '''
     test_cases = {
-        'Artist Name - 2010 - Album Title' : AlbumInfo(
+        'Artist Name - 2010 - Album Title' : 
+        AlbumInfo(
           artist_name = 'Artist Name', 
           year = '2010', 
           album_title = 'Album Title',
           misc = None,
           length = None),
-        'Artist Name - 2010 - Album Title (Misc)' : AlbumInfo(
+
+        'Artist Name - 2010 - Album Title (Misc)' : 
+        AlbumInfo(
           artist_name = 'Artist Name', 
           year = '2010', 
           album_title = 'Album Title',
           misc = 'Misc',
           length = None),
-        'Artist Name - 2010 - Album Title EP' : AlbumInfo(
+
+        'Artist Name - 2010 - Album Title EP' : 
+        AlbumInfo(
           artist_name = 'Artist Name', 
           year = '2010', 
           album_title = 'Album Title',
